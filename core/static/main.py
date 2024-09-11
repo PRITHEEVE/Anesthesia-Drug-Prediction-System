c929b988b83b532a6974f0dc27cb68e13c6bb9f5
PRITHEEVE D <128456613+PRITHEEVE@users.noreply.github.com>
2024-09-11 12:49:49 +0530

    Create main.css

---
+++ core/static/main.py
@@ -0,0 +1,117 @@
+*{
+	padding: 0;
+	margin: 0;
+}
+body{
+	background-size: cover;
+	align-items: center;
+	justify-content: center;
+	display: flex;
+	font-family: sans-serif;
+    background-color: bisque;
+
+}
+
+.selection{
+    padding-left: 26%;
+}
+.selectbutton{
+    height:60px ;
+    width: 300px;
+}
+h1{
+    text-align: center;
+    padding-top: 5%;
+    font-family:'Times New Roman', Times, serif;
+    color: #0c0c53;
+    font-weight: 900;
+    font-size: x-large;
+}
+h2{
+    text-align: center;
+    font-family:'Times New Roman', Times, serif;
+    color: rgb(12, 12, 83);
+    font-size: larger   ;
+    font-weight: 900;
+}
+
+
+input{
+    height: 30px;
+    width: 300px;
+    border: 1px;
+    border-radius: 15px;
+    text-align: center;
+    font-size:larger;
+    font-weight: 900;
+    color: black;
+    
+}
+select{
+    height: 30px;
+    width: 300px;
+    border: 1px;
+    border-radius: 15px;
+    text-align: center;
+    font-size:larger;
+    font-weight: 900;
+    color: black;
+
+}
+option{
+    height: 30px;
+    width: 300px;
+    border: 1px;
+    border-radius: 15px;
+    text-align: center;
+    font-size:larger;
+    font-weight: 900;
+    color: black;
+
+}
+th{
+    padding: 3%;
+}
+button{
+    border: 1px;
+    border-radius: 15px;
+    background-color: rgb(121, 45, 45);
+    color: aquamarine;
+    font-size: large;
+    width: 100px;
+    height: 30px;
+}
+.button{
+    text-align: center;
+    
+
+}
+h3{
+    text-align: center;
+    font-size: xx-large;
+    font-weight: 700;
+    font-family: 'Times New Roman', Times, serif;
+    color: rgb(77, 20, 20);
+}
+.author{
+    display: flex;
+}
+h4{
+    text-align: center;
+    padding: 8%;
+    color:rgb(12, 12, 83) ;
+    font-size: large;
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: 800;
+
+}
+h5{
+    
+    color: rgb(12, 12, 83);
+
+    font-family: cursive;
+    font-weight: 800;
+}
+.img{
+    text-align: center;
+}
